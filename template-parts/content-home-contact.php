cba82be31646624159bf351a770d6aa39eff1934
Brenden <brendenwalters42@gmail.com>
2021-03-13 18:27:34 -0500

    contact form updates

--- template-parts/content-home-contact.php
+++ template-parts/content-home-contact.php
@@ -4,11 +4,12 @@
 
 <section class="home-contact-form-section">
   <div class="content-container">
-    <div class="home-contact-image">
-    </div>
-
     <div class="home-contact-form">
       <?php echo do_shortcode("[contact-form-7 id=\"106\" title=\"Contact form 1\"]") ?>
+    </div>
+
+    <div class="home-contact-image">
+      <img src="https://via.placeholder.com/2000" alt="">
     </div>
   </div>
   
