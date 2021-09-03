fb57ba658158e7b93a117608a9dd325e045368ff
Brenden Walters <brendenwalters42@gmail.com>
2021-09-03 13:21:21 -0400

    Fixed home page template part imports

--- template-home.php
+++ template-home.php
@@ -10,15 +10,15 @@ get_header();
 
 		<!-- content -->
 		<?php
-			get_template_part('template-parts/content-home','hero');
-			get_template_part('template-parts/content-home','video');
-			get_template_part('template-parts/content-home','benefits');
-			get_template_part('template-parts/content-home','programs');
-			get_template_part('template-parts/content-home','about');
-			get_template_part('template-parts/content-home','cta-banner');
-			get_template_part('template-parts/content-home','testimonials');
-			get_template_part('template-parts/content-home','contact');
-			get_template_part('template-parts/content-home','ig-feed');
+			get_template_part('template-parts/home-page/content-home','hero');
+			get_template_part('template-parts/home-page/content-home','video');
+			get_template_part('template-parts/home-page/content-home','benefits');
+			get_template_part('template-parts/home-page/content-home','programs');
+			get_template_part('template-parts/home-page/content-home','about');
+			get_template_part('template-parts/home-page/content-home','cta-banner');
+			get_template_part('template-parts/home-page/content-home','testimonials');
+			get_template_part('template-parts/home-page/content-home','contact');
+			get_template_part('template-parts/home-page/content-home','ig-feed');
 		?>
 
 	</main><!-- #main -->
