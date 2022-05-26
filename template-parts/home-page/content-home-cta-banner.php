bf95091c441766d3db2b982361c7dc5003c09970
Brenden <brendenwalters42@gmail.com>
2022-05-26 14:53:14 -0400

    Updates to home page banner
    Stack on mobile. Redner ACF values to page

--- template-parts/home-page/content-home-cta-banner.php
+++ template-parts/home-page/content-home-cta-banner.php
@@ -1,14 +1,24 @@
 <?php
-
+  $banner_title      =get_field('banner_title');
+  $banner_cta_text   =get_field('banner_button_text');
+  $banner_cta_link   =get_field('banner_button_link');
+  $banner_image      =get_field('banner_image');
 ?>
 
-<section class="home-cta-banner" data-uk-parallax="{bg: '-200'}" style="background-image: url('https://images.unsplash.com/photo-1489641493513-ba4ee84ccea9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')">
-  <div class="home-banner-container">
-    <div class="banner-text">
-      <h2>Lorem ipsum dolor sit.</h2>
+<?php if (!empty($banner_image)){ ?>
+<section class="home-cta-banner" uk-parallax="bgy: -200" style="background-image: url('<?php echo($banner_image['url']); ?>')">
+<?php } else { ?>
+<section class="home-cta-banner no-image">
+<?php } ?>
+  <div class="home-banner-container" uk-scrollspy="target: > div; cls:uk-animation-fade; delay: 100">
+    <div class="banner-text" uk-scrollspy-class="uk-animation-slide-left-small">
+      <h2><?php echo((empty($banner_title)) ? (acf_get_field('banner_title')['default_value']) : $banner_title ); ?></h2>
+      <?php if (!empty($banner_text)): ?>
+        <p><?php echo($banner_text) ?></p>
+      <?php endif; ?>
     </div>
-    <div class="cta-container">
-      <a href="" class="cta-primary">Learn More</a>
+    <div class="cta-container" uk-scrollspy-class="uk-animation-slide-right-small">
+      <a href="<?php echo($banner_cta_link); ?>" class="cta-primary"><?php echo($banner_cta_text ? $banner_cta_text : (acf_get_field('banner_button_text')['default_value'])); ?></a>
     </div>
   </div>
 </section>
