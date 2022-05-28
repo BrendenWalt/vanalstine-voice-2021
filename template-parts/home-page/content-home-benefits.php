2faf3d2f85f4ee4a33b597bb35a020fab7e73c22
Brenden <brendenwalters42@gmail.com>
2022-05-28 15:36:25 -0400

    Revamped styles for the benfits cards

--- template-parts/home-page/content-home-benefits.php
+++ template-parts/home-page/content-home-benefits.php
@@ -4,10 +4,13 @@
 ?>
 
 <section class="home-benefits">
+  <div>
+    <h2>Lorem Ipsum</h2>
+  </div>
   <div class="home-benefits-container">
     <div uk-scrollspy="target: > div; cls:uk-animation-fade uk-animation-slide-bottom-medium; delay: 200" class="home-benefits-cards-container">
       <!-- Card 1 -->
-      <div class="home-benefit-card">
+      <div class="home-benefit-card  uk-card uk-card-default uk-card-body">
         <div class="home-benefit-icon">
           <img src="" alt="">
         </div>
@@ -17,9 +20,9 @@
         </div>
       </div>
       <!-- Card 1 -->
-      <div  class="home-benefit-card">
+      <div  class="home-benefit-card uk-card uk-card-default uk-card-body">
         <div class="home-benefit-icon">
-          <img src="" alt="">
+        <i class="fas fa-music"></i>
         </div>
         <div class="home-benefit-card-text">
           <h3>Lorem ipsum dolor sit</h3>
@@ -27,7 +30,7 @@
         </div>
       </div>
       <!-- Card 1 -->
-      <div  class="home-benefit-card">
+      <div  class="home-benefit-card uk-card uk-card-default uk-card-body">
         <div class="home-benefit-icon">
           <img src="" alt="">
         </div>
@@ -39,8 +42,8 @@
     </div>
     
     <div class="home-benefits-ctas">
-      <a href="" class="cta-secondary">Lorem, ipsum</a>
-      <a href="" class="cta-secondary">Lorem, ipsum</a>
+      <a href="" class="cta-primary">Lorem, ipsum</a>
+      <a href="" class="cta-primary">Lorem, ipsum</a>
     </div>
   </div>
  
