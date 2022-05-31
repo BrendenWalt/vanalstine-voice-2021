079d96575b6334d05d5b98c44d5918a9cf4c05a5
Brenden <brendenwalters42@gmail.com>
2022-05-30 20:14:15 -0400

    syntax cleanup

--- template-parts/home-page/content-home-benefits.php
+++ template-parts/home-page/content-home-benefits.php
@@ -10,7 +10,7 @@
   <div class="home-benefits-container">
     <div uk-scrollspy="target: > div; cls:uk-animation-fade uk-animation-slide-bottom-medium; delay: 200" class="home-benefits-cards-container">
       <!-- Card 1 -->
-      <div class="home-benefit-card  uk-card uk-card-default uk-card-body">
+      <div class="home-benefit-card uk-card uk-card-default uk-card-body">
         <div class="home-benefit-icon">
           <img src="" alt="">
         </div>
