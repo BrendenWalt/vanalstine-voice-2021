51a99fe8c1489709ac051167ec520f43fc4322b6
Brenden <brendenwalters42@gmail.com>
2022-05-31 17:26:40 -0400

    Various minor style updates

--- template-parts/home-page/content-home-programs.php
+++ template-parts/home-page/content-home-programs.php
@@ -7,7 +7,7 @@
   <div class="home-programs-container">
 
     <!-- program card -->
-    <div class="home-program-card" style="background-image: url('https://images.unsplash.com/photo-1540593463874-59835505e99d')">
+    <div class="home-program-card uk-card uk-body uk-card-default" style="background-image: url('https://images.unsplash.com/photo-1540593463874-59835505e99d')">
       <div class="program-text">
         <h3 class="program-title">Lorem, ipsum.</h3>
         <p class="program-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, repellat!</p>
@@ -16,7 +16,7 @@
     </div>
 
     <!-- program card -->
-    <div class="home-program-card" style="background-image: url('https://images.unsplash.com/photo-1483000805330-4eaf0a0d82da')">
+    <div class="home-program-card uk-card uk-body uk-card-default" style="background-image: url('https://images.unsplash.com/photo-1483000805330-4eaf0a0d82da')">
       <div class="program-text">
         <h3 class="program-title">Lorem, ipsum.</h3>
         <p class="program-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, repellat!</p>
@@ -25,7 +25,7 @@
     </div>
 
     <!-- program card -->
-    <div class="home-program-card" style="background-image: url('https://images.unsplash.com/photo-1543062591-e3c0fdb97350')">
+    <div class="home-program-card uk-card uk-body uk-card-default" style="background-image: url('https://images.unsplash.com/photo-1543062591-e3c0fdb97350')">
       <div class="program-text">
         <h3 class="program-title">Lorem, ipsum.</h3>
         <p class="program-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error id, aliquid iusto illum ipsa quam saepe debitis voluptatibus natus doloremque quaerat voluptatum quos repellat officia modi fugiat accusantium nihil vel totam ipsam sunt, corporis sed? Tempora praesentium consequatur cum maxime sint unde vero aut, iste odio, officia ducimus sit temporibus.</p>
