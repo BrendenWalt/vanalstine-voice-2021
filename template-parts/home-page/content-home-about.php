93eabe2c832a21411163f86529dbd9d277d901d9
Brenden <brendenwalters42@gmail.com>
2022-05-31 17:26:04 -0400

    Home about section style updates

--- template-parts/home-page/content-home-about.php
+++ template-parts/home-page/content-home-about.php
@@ -3,7 +3,7 @@
 ?>
 
 <section class="home-about">
-  <div class="content-container">
+  <div class="content-container" uk-scrollspy="target: .home-about-text-container;cls:uk-animation-fade uk-animation-slide-right-medium; delay: 300">
     <div class="home-about-img">
       <?php if(!empty($about_image)) { ?>
         <img src="https://via.placeholder.com/620x720" alt="">
@@ -11,12 +11,15 @@
         <img src="https://via.placeholder.com/620x720/ff00ff" alt="">
       <?php } ?>
     </div>
-    <div class="home-about-text-container">
+    <div class="home-about-text-container uk-card uk-card-body uk-card-default uk-animation-slide-right uk-animation-fade">
       <div class="about-box">
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque vero rerum sit similique nemo a ducimus, architecto cum quos asperiores fugiat hic ut voluptates optio, assumenda voluptatum velit aspernatur at?</p>
       </div>
-      <a class="cta-primary color-secondary">Learn More</a>
+      <div class="about-cta-container">
+        <a class="cta-primary color-secondary">Learn More</a>
+      </div>
+      
     </div>
   </div>
 </section>
