14d789eb91467e058a2b4054505b6e67f2ce840e
Brenden <brendenwalters42@gmail.com>
2022-06-02 20:30:31 -0400

    Testimonial query in progreess

--- template-parts/home-page/content-home-testimonials.php
+++ template-parts/home-page/content-home-testimonials.php
@@ -1,80 +1,54 @@
+<!-- Query Testimonial posts -->
 <?php
+  $args = array(
+    'post_type' => 'testimonials',
+    'post_status' => 'publish',
+    'orderby' => 'ID',
+    'order' => 'ASC'
+  );
 
+  $loop = new WP_Query( $args );
+
+  if ($loop->have_posts()) {
 ?>
+  <section class="home-testimonials">
+    <div class="carousel-container">
+      <div class="content-container swiper-container">
+        <div class="testimonials-container swiper-wrapper">
 
-<section class="home-testimonials">
+    <?php
+      while ( $loop->have_posts() ) : $loop->the_post();
+    ?>
 
-  <?php if( have_rows('testimonial_tab_group')): 
-    $i = 1;
-    
-    while ( have_rows('testimonial_tab_group') ) : the_row();
-      while ( have_rows('testimonial_1')) : the_row();
-      $text = get_sub_field('testimonial_text');
-  ?>
+          <blockquote class="testimonial-card-container swiper-slide">
+            <div class="slide-content">
+              <h4><?php echo the_field('testimonial_text') ?></h4>
+              <footer>
+                <strong><?php echo the_field('testimonial_name') ?></strong><br>
+                <i><?php echo the_field('testimonial_subtitle') ?></i>
+              </footer>
+            </div>
+          </blockquote>
 
-        <div>
-         <?php echo($text); ?>
+    <?php
+      endwhile;
+      wp_reset_postdata();
+    ?>
+
+        <div class="testimonial-counter-container swiper-pagination">
+          <!-- <span class="testimonial-counter active"></span>
+          <span class="testimonial-counter"></span> -->
         </div>
-        <?php  endwhile; ?>
-    <?php endwhile; ?>
+        <div class="testimonial-nav arrow-right swiper-button-next">
+          <!-- <i class="fas fa-chevron-right"></i> -->
+        </div>
+        <div class="testimonial-nav arrow-left swiper-button-prev">
+          <!-- <i class="fas fa-chevron-left"></i> -->
+        </div>
 
-    <?php endif; ?>
-
-  <div class="carousel-container">
-    <div class="content-container swiper-container">
-      
-      <div class="testimonials-container swiper-wrapper">
-
-      
-        <!-- Testimonial -->
-        <blockquote class="testimonial-card-container swiper-slide">
-          <div class="slide-content">
-            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis blanditiis vitae in veritatis aut nulla minima consectetur impedit consequatur culpa!</h4>
-            <footer>
-              <strong>Lorem ipsum</strong><br>
-              <i>Lorem ipsum</i>
-            </footer>
-          </div>
-        </blockquote>
-
-        <!-- Testimonial -->
-        <blockquote class="testimonial-card-container swiper-slide">
-          <div class="slide-content">
-            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis blanditiis vitae in veritatis aut nulla minima consectetur impedit consequatur culpa!</h4>
-            <footer>
-              <strong>Lorem ipsum</strong><br>
-              <i>Lorem ipsum</i>
-            </footer>
-          </div>
-        </blockquote>
-
-        <!-- Testimonial -->
-        <blockquote class="testimonial-card-container swiper-slide">
-          <div class="slide-content">
-            <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis blanditiis vitae in veritatis aut nulla minima consectetur impedit consequatur culpa!</h4>
-            <footer>
-              <strong>Lorem ipsum</strong><br>
-              <i>Lorem ipsum</i>
-            </footer>
-          </div>
-        </blockquote>
       </div>
-
-      <div class="testimonial-counter-container swiper-pagination">
-        <!-- <span class="testimonial-counter active"></span>
-        <span class="testimonial-counter"></span> -->
-      </div>
-      <div class="testimonial-nav arrow-right swiper-button-next">
-        <!-- <i class="fas fa-chevron-right"></i> -->
-      </div>
-      <div class="testimonial-nav arrow-left swiper-button-prev">
-        <!-- <i class="fas fa-chevron-left"></i> -->
-      </div>
-      
-      
-      
-
     </div>
-  </div>
-  
+
 </section>
+
+<?php } ?>
