8960bcea57184be4484d86bdbb48aeb28baa52d5
Brenden <brendenwalters42@gmail.com>
2022-06-13 19:05:38 -0400

    removed iFrame code from video section

--- template-parts/home-page/content-home-video.php
+++ template-parts/home-page/content-home-video.php
@@ -7,8 +7,6 @@
   <div class="home-video-container content-container">
     <div class="video">
       <?php the_field('home_video'); ?>
-      <!-- iframe -->
-      <!-- <iframe width="100%" height="100%" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: 100%;" allowfullscreen></iframe> -->
     </div>
     <div class="video-textarea">
       <?php the_field('video_text'); ?>
