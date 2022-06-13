d4acca3344834b2010de6575bf4b7bbd1e0d5d92
Brenden <brendenwalters42@gmail.com>
2022-06-13 18:29:28 -0400

    Add an H2 to the contact section

--- template-parts/home-page/content-home-contact.php
+++ template-parts/home-page/content-home-contact.php
@@ -3,8 +3,14 @@
 ?>
 
 <section class="home-contact-form-section">
+
   <div class="content-container">
     <div class="home-contact-form">
+    <div class="contact-title-container">
+      <h2>
+        Ready To Learn More?
+      </h2>
+    </div>
       <?php echo do_shortcode("[contact-form-7 id=\"106\" title=\"Contact form 1\"]") ?>
     </div>
 
