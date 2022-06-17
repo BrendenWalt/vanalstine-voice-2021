21fbf5ce95f9a06b532e8f7c38553e52cb3de852
Brenden <brendenwalters42@gmail.com>
2022-06-17 13:26:25 -0400

    Updated styles for contact form

--- template-parts/home-page/content-home-contact.php
+++ template-parts/home-page/content-home-contact.php
@@ -6,12 +6,18 @@
 
   <div class="content-container">
     <div class="home-contact-form">
-    <div class="contact-title-container">
-      <h2>
-        Ready To Learn More?
-      </h2>
-    </div>
-      <?php echo do_shortcode("[contact-form-7 id=\"106\" title=\"Contact form 1\"]") ?>
+      <div class="contact-title-container">
+        <h2>
+          Ready To Learn More?
+        </h2>
+      </div>
+
+      <div class="contact-form-container uk-card uk-card-default">
+        <div class="uk-card-body">
+         <?php echo do_shortcode("[contact-form-7 id=\"106\" title=\"Contact form 1\"]") ?>
+        </div>
+      </div>
+      
     </div>
 
     <div class="home-contact-image">
