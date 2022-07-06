e2a75105ac404c3f4f4377c9b34c44a6a44ecc58
Brenden <brendenwalters42@gmail.com>
2022-07-06 16:12:35 -0400

    starting the standard page template

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+		<h1>HELLO from page.php</h1>
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -34,5 +34,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
