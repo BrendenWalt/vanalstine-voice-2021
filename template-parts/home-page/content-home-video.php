4b893d22427869e9f689425db9dde4dfe5ea986b
Brenden <brendenwalters42@gmail.com>
2022-08-23 21:26:15 -0400

    Add "no-video" class on video intro section if no video is present

--- template-parts/home-page/content-home-video.php
+++ template-parts/home-page/content-home-video.php
@@ -5,11 +5,13 @@
 
 <section class="home-video" uk-scrollspy="target: .scroll-to-top; cls: uk-animation-fade; delay: 300">
   <a id="home-section-intro" class="home-anchor"></a>
-  <div class="home-video-container content-container">
+  <div class="home-video-container content-container <?php echo((empty($video)) ? 'no-video' : ''); ?>">
+    <?php if(!empty($video)): ?>
     <div class="video">
       <?php the_field('home_video'); ?>
     </div>
-    <div class="video-textarea">
+    <?php endif; ?>
+    <div class="video-textarea<?php echo((empty($video)) ? ' uk-text-center' : ''); ?>">
       <?php the_field('video_text'); ?>
     </div>
   </div>
